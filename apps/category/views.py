d44cee848eeef7a1b60ecf5a13cc5b39cfd12f5c
Umida <islamovaumida12@gmail.com>
2022-06-08 07:57:53 -0400

    update product/category

--- apps/category/views.py
+++ apps/category/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
+
 from apps.category.models import Category
 from apps.category.permissions import IsAdminOrAllowAny
 from apps.category.serializers import CategorySerializer
--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from apps.product.views import ListCreateProductView, GetProductView, LikeProductView, DestroyProductView
+
+from apps.product.views import * 
 
 
 urlpatterns = [
     path('', ListCreateProductView.as_view()),
     path('list_or_create/', ListCreateProductView.as_view()),
+    path('update/<int:pk>/', UpdateProductView.as_view()),
     path('<int:pk>/', GetProductView.as_view()),
     path('<int:pk>/like/', LikeProductView.as_view()),
     path('delete/<int:pk>/', DestroyProductView.as_view()),
+
     
 
 ]
